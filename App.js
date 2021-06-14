5a1ea7b9838f1f929ed4208aa6956ec1620270f6
sjkim <ksj911011@gmail.com>
2021-06-15 00:59:12 +0900

    feat: 22day

--- App.js
+++ App.js
@@ -1,8 +1,30 @@
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
+import {Ionicons} from "@expo/vector-icons";
+import * as Font from "expo-font";
+import {Asset} from "expo-asset";
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+  const preload = () => {
+      const fontsToLoad = [Ionicons.font];
+      const fontPromises = fontsToLoad.map((font)=>Font.loadAsync(font));
+      const imagesToLoad = [require("./assets/favicon.png")];
+      const imagePromises = imagesToLoad.map(image=>Asset.loadAsync(image))
+      return Promise.all([...fontPromises,...imagePromises]);
+  } 
+  if(loading){
+    return(
+        <AppLoading
+            startAsync={preload}
+            onError={console.warn}
+            onFinish={onFinish}
+        />
+    ) 
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
